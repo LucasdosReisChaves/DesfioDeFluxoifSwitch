1a3973304fe8bd75f2fd318eb3d6e00e3d8a1365
Lucas Chaves <Lucas.rchaves@yahoo.com.br>
2018-04-19 11:42:18 -0300

    Interação com com usuário

--- OperacoesCondicionais/Program.cs
+++ OperacoesCondicionais/Program.cs
@@ -15,7 +15,29 @@ namespace OperacoesCondicionais
              
              //Declaração das variáveis n1 e n2. Elas serão do tipo double
              double n1, n2;
-             
+
+            /*
+            Para limpar a tela de comando a cada execução, utilizaremos o comando Console.Clear();
+            obs.:Clear->Limpar
+            
+            */
+            Console.Clear();
+            Console.WriteLine("Escolha uma das operções abaixo:");
+            /*Para a exibição das opções de operação, vamos usar  o comando Console.WriteLine e, nele usaremos um comandos de quebra de line \n(contra-barra n(new line)).
+            Assim, as opções ficarão uma abaixo da outra.
+            */
+            Console.WriteLine("n1-soma\n2-subtrair\n3-multiplicar\n4-dvidir");
+            operacao = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um número:");
+            n1 = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite outro número");
+            n2 = double.Parse(Console.ReadLine());
+
+            
+
+
         }
     }
 }
