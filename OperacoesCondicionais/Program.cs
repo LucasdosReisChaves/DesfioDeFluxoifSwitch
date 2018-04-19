f6b93e0e1b21d4bad08964e595ad71446e43a57d
Lucas Chaves <Lucas.rchaves@yahoo.com.br>
2018-04-19 12:10:24 -0300

    Código completo

--- OperacoesCondicionais/Program.cs
+++ OperacoesCondicionais/Program.cs
@@ -26,7 +26,7 @@ namespace OperacoesCondicionais
             /*Para a exibição das opções de operação, vamos usar  o comando Console.WriteLine e, nele usaremos um comandos de quebra de line \n(contra-barra n(new line)).
             Assim, as opções ficarão uma abaixo da outra.
             */
-            Console.WriteLine("n1-soma\n2-subtrair\n3-multiplicar\n4-dvidir");
+            Console.WriteLine("1-soma\n2-subtrair\n3-multiplicar\n4-dividir");
             operacao = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Digite um número:");
@@ -35,7 +35,31 @@ namespace OperacoesCondicionais
             Console.WriteLine("Digite outro número");
             n2 = double.Parse(Console.ReadLine());
 
-            
+        if(operacao==1)
+        {
+            Console.WriteLine("O resulttado da soma é:"+(n1+n2));
+        }
+        else if (operacao==2)
+        {
+            Console.WriteLine("O resultado da subtração é:"+(n1-n2));
+        }
+        else if(operacao==3)
+        {
+            Console.WriteLine("O resultado da multiplicação é: "+(n1*n2));
+        }
+        else if(operacao==4)
+        {
+            Console.WriteLine("O resultado da divição é: "+(n1/n2));
+        }
+        else  
+        {
+            Console.WriteLine("Operação inválida");
+        }
+
+
+        
+
+
 
 
         }
