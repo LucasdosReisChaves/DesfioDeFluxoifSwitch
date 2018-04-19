5971e85028804b2d8174dce04d496ccee1e5047d
Lucas Chaves <Lucas.rchaves@yahoo.com.br>
2018-04-19 11:11:20 -0300

    Declaração de variáveis

--- OperacoesCondicionais/Program.cs
+++ OperacoesCondicionais/Program.cs
@@ -6,7 +6,16 @@ namespace OperacoesCondicionais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //comentario in-line(em linha)
+            /*
+               Comentario in-block(em bloco) 
+             */
+             //Declaração da variável da operação. Ela será do tipo inteira 
+             int operacao;
+             
+             //Declaração das variáveis n1 e n2. Elas serão do tipo double
+             double n1, n2;
+             
         }
     }
 }
